b6bd82ee32af359873d67ec762a5f1910eb26d9c
Catherine Loesch <catherine.loesch@protonmail.com>
2021-02-14 04:37:41 +0000

    learn spec/02_shoe_spec.rb submitted successfully

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -1,1 +1,25 @@
 # Make your shoe class here!
+
+class Shoe
+attr_accessor :brand, :color, :size, :material, :condition
+
+def initialize(brand)
+    @brand = brand
+end
+
+def cobble
+    @condition = "new"
+    puts "Your shoe is as good as new!"
+end
+
+end
+
+
+my_shoe = Shoe.new("uggs")
+
+my_shoe.color = "brown"
+my_shoe.size = 3
+my_shoe.material = "faux leather"
+my_shoe.condition = "old"
+
+my_shoe.cobble
